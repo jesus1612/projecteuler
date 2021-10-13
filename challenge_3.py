c003c5833ca377fe40296499ca8def25abcca1df
Genaro Melgar <gjmg1612@gmail.com>
2021-10-12 23:30:16 -0500

    Añadida pequeña modificacion sobre codigo

--- challenge_3.py
+++ challenge_3.py
@@ -2,9 +2,13 @@
 # What is the largest prime factor of the number 600851475143?
 
 # Find the prime factors
-num = 13195
-
-for i in range(int(num/2) + 1, 0, -1):
+num = 600851475143
+counter = 0
+for i in range(num + 1, 2, -1):
   if num % i == 0:
-    print(i)
-    break
+    counter = 1
+    for j in range (int(i/2) +1,2,-1 ):
+      if i % j == 0:
+        counter = 0
+    if counter == 1:
+      print(i)
